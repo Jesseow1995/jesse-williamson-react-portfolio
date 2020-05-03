dd804bc91af375151b81b16ec2d2462ab1927c9f
Jesse Williamson <jesseowilliamson@gmail.com>
2020-05-03 12:59:33 -0600

    Starting developing Auth

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import loginImage from "../../../static/assets/images/auth/manchas.jpg"
+import Login from "../auth/login";
+
 
 export default class Auth extends Component {
     render() {
         return (
-            <div>
-                Auth...
+            <div className="auth-page-wrapper">
+                <div
+                    className="left-column"
+                    style={{
+                        backgroundImage: `url(${loginImage})`
+                    }}>
+
+                </div>
+
+                <div className="right-column">
+                    <Login />
+                </div>
+
             </div>
         )
     }
