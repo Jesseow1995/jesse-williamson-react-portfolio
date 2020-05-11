55196a2863b7515fce5f66851c474325fd0830d0
Jesse Williamson <jesseowilliamson@gmail.com>
2020-05-11 11:28:33 -0600

    saving progress

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -7,6 +7,7 @@ export default class Login extends Component {
     this.state = {
       email: "",
       password: "",
+      errorText: ""
     }
 
     this.handleChange = this.handleChange.bind(this);
@@ -15,7 +16,8 @@ export default class Login extends Component {
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     })
   }
 
@@ -31,7 +33,21 @@ export default class Login extends Component {
         { withCredentials: true }
       ).
       then(response => {
-        console.log("response", response);
+        if (response.data.status === "created") {
+          this.props.handleSuccessfulAuth();
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          })
+          this.props.handleUnsuccessfulAuth();
+
+        }
+      }).catch(error => {
+
+        this.setState({
+          errorText: "An error occured"
+        })
+        this.props.handleUnsuccessfulAuth();
       });
 
     event.preventDefault();
@@ -44,6 +60,9 @@ export default class Login extends Component {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
+
         <form onSubmit={this.handleSubmit}>
           <input
             type="email"
