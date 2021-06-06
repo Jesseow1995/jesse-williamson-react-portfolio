735b3de0eec9d0dd6fd1afe517a596e574672c28
Jesse Williamson <jesseowilliamson@gmail.com>
2021-06-06 15:11:52 -0600

    fixing a bug

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-
-app.use(cors())
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function (req, res) {
   res.sendFile(__dirname + '/dist/index.html');
